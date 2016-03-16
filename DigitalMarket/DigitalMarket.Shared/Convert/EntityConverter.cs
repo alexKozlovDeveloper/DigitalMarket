0915c0f9c922bb623fc41bb1475897aed4845faf
Aliaksei Kazlou <aliaksei_kazlou@epam.com>
2016-03-16 15:09:56 +0300

    add project DigitalMarket.Shared

---
+++ DigitalMarket/DigitalMarket.Shared/Convert/EntityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalMarket.Shared.Convert
+{
+    public class EntityConverter
+    {
+        public static T Convert<T, TS>(TS item) where T : new()
+        {
+            var result = new T();
+
+            var fields = item.GetType().GetProperties();
+
+            foreach (var propertyInfo in fields)
+            {
+                var resProp = result.GetType().GetProperty(propertyInfo.Name);
+
+                if (propertyInfo.PropertyType == resProp.PropertyType)
+                {
+                    var value = propertyInfo.GetValue(item);
+
+                    result.GetType().GetProperty(propertyInfo.Name).SetValue(result, value);
+                }
+            }
+
+            return result;
+        }
+    }
+}
